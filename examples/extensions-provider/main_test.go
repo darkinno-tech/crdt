ce50ba56a79447552b5a940a73fc12fdcd3d35f6
im10furry <xiaoliuwj.china@gmail.com>
2026-07-29 18:53:16 +0800

    feat(examples): add opt-in relay provider

---
+++ examples/extensions-provider/main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"testing"
+)
+
+type failingWriter struct{}
+
+func (failingWriter) Write([]byte) (int, error) { return 0, io.ErrClosedPipe }
+
+func TestRun(t *testing.T) {
+	var output bytes.Buffer
+	if err := run(&output); err != nil {
+		t.Fatal(err)
+	}
+	const want = "websocket_to_http=2\nhttp_to_websocket=5\nrelay=5\n"
+	if output.String() != want {
+		t.Fatalf("run output = %q, want %q", output.String(), want)
+	}
+}
+
+func TestRunReturnsWriteFailure(t *testing.T) {
+	if err := run(failingWriter{}); !errors.Is(err, io.ErrClosedPipe) {
+		t.Fatalf("run error = %v, want wrapped %v", err, io.ErrClosedPipe)
+	}
+}
