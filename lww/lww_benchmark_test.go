1c3e55d91fa9c14c8ab67936c98ace3c0b547b37
im10furry <xiaoliuwj.china@gmail.com>
2026-07-29 14:38:01 +0800

    test(lww): verify framed set replication

--- lww/lww_benchmark_test.go
+++ lww/lww_benchmark_test.go
@@ -75,3 +75,52 @@ func BenchmarkMapMarshalTenThousandKeys(b *testing.B) {
 		}
 	}
 }
+
+func BenchmarkSetApplyDeltaTenElements(b *testing.B) {
+	source, err := NewSet[string]("source")
+	if err != nil {
+		b.Fatal(err)
+	}
+	var combined SetDelta[string]
+	for index := 0; index < 10; index++ {
+		delta, err := source.AddWithDelta("element-" + strconv.Itoa(index))
+		if err != nil {
+			b.Fatal(err)
+		}
+		combined, err = combined.Merge(delta)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for index := 0; index < b.N; index++ {
+		target, err := NewSet[string]("target")
+		if err != nil {
+			b.Fatal(err)
+		}
+		if err := target.ApplyDelta(combined); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkSetMarshalTenThousandElements(b *testing.B) {
+	codec := setStringCodec{id: "example.com/lww-set-benchmark/v1"}
+	value, err := NewSet[string]("source")
+	if err != nil {
+		b.Fatal(err)
+	}
+	for index := 0; index < 10_000; index++ {
+		if err := value.Add("element-" + strconv.Itoa(index)); err != nil {
+			b.Fatal(err)
+		}
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for index := 0; index < b.N; index++ {
+		if _, err := value.MarshalBinary(codec); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
